d747bb0520b9d1b48d8d015e9ccefbc164b13a75
sofiialives <127687828+sofiialives@users.noreply.github.com>
2023-06-04 21:17:22 +0300

    56

---
+++ js/modal4.js
@@ -0,0 +1,14 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open-4]"),
+      closeModalBtn: document.querySelector("[data-modal-close-4]"),
+      modal: document.querySelector("[data-modal-4]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+  })();
