1b50def8b519b3bb95c3770539bff76646e7e7b7
sofiialives <127687828+sofiialives@users.noreply.github.com>
2023-06-11 23:19:54 +0300

    3

--- js/touch.js
+++
@@ -1,7 +0,0 @@
-// const box = document.querySelector('.main');
-// document.addEventListener('click', (e) => {
-//   const click = e.composedPath().includes(box);
-//   if ( !click ) {
-//     box.style.display = 'none';
-//   }
-// })
