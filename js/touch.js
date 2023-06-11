1403ffcd7fccf5e57ae3ecdf4658319d7709f052
sofiialives <127687828+sofiialives@users.noreply.github.com>
2023-06-11 22:59:31 +0300

    1

--- js/touch.js
+++ js/touch.js
@@ -1,15 +1,7 @@
-// Get the overlay element and the picture element
-const overlay = document.getElementById('overlay');
-const picture = document.getElementById('picture');
-
-// Add a click event listener to the picture
-picture.addEventListener('click', toggleOverlay);
-
-// Function to toggle the overlay visibility
-function toggleOverlay() {
-  if (overlay.style.display === 'none') {
-    overlay.style.display = 'block';
-  } else {
-    overlay.style.display = 'none';
-  }
-}
+// const box = document.querySelector('.main');
+// document.addEventListener('click', (e) => {
+//   const click = e.composedPath().includes(box);
+//   if ( !click ) {
+//     box.style.display = 'none';
+//   }
+// })
